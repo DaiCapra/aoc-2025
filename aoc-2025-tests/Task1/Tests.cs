9fcf8c5fef06ea0e550c832a9cd92f7f54646c1f
Daniel Artmann <daniel@artmann.co>
2025-11-28 18:31:36 +0100

    first

---
+++ aoc-2025-tests/Task1/Tests.cs
@@ -0,0 +1,15 @@
+namespace aoc_2025_tests.Task1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ aoc-2025/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace aoc_2025;
+
+public class Program
+{
+    public static void Main()
+    {
+        var program = new Program();
+        program.Run();
+    }
+
+    private void Run()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
