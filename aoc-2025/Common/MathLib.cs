e8f6a28645136f3f708ed94a470ab37903545ac2
Daniel Artmann <daniel@artmann.co>
2025-12-01 23:35:09 +0100

    cleanup

--- aoc-2025/Common/MathLib.cs
+++ aoc-2025/Common/MathLib.cs
@@ -3,8 +3,9 @@
 public static class MathLib
 {
     // https://stackoverflow.com/a/1082938
-    public static int Mod(int x, int m) {
-        int r = x%m;
-        return r<0 ? r+m : r;
+    public static int Mod(int x, int m)
+    {
+        int r = x % m;
+        return r < 0 ? r + m : r;
     }
 }
